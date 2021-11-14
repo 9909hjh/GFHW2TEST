ba9c885736d3503e428484cd558647391112cb77
한주형 <80089238+9909hjh@users.noreply.github.com>
2021-11-14 06:24:57 +0000

    이동수정

--- Player.cpp
+++ Player.cpp
@@ -20,18 +20,30 @@ void Player::clean() {}
 
 void Player::handleInput()
 {
-  if(TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_RIGHT))
+  m_velocity.setX(0);
+  if(m_position.getX() < 0 || m_position.getX() + 82 >= 720)
   {
-    m_velocity.setX(2);
+    //std::cout <<"화면 밖으로 나감" << "\n";
+    if(m_position.getX() < 0) // 화면 밖으로 나가도 움직이게 하는 코드
+      m_velocity.setX(5);
+    else if(m_position.getX() + 82 >= 720)
+      m_velocity.setX(-5); // 기존에는 if문 밖에 있는 m_velocity.setX(0);때문에 못 움직여졌음
+    // if(m_position.getX() < 0)
+    //   m_position.setX(5);
+    // else
+    //   m_position.setX(-5); // 이렇게만 쓰면 캐릭터가 화면안에서 루프되는것 처럼보인다.
   }
-  else if(TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_LEFT))
-  {
-    m_velocity.setX(-2);
-  } 
-  else 
-  {
-    m_velocity.setX(0);
+  else{
+    if(TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_RIGHT))
+    {
+      m_velocity.setX(5);
+    }
+    else if(TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_LEFT))
+    {
+      m_velocity.setX(-5);
+    }
   }
+  
 
   if(TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_UP))
   {
